ccab7ab85f3ce7038507720ec870d63b03bb75aa
Philipp Eppelt <philipp88@eppelt.de>
2013-06-19 10:58:33 +0200

    Cpu dependent virtLayer header file created

---
+++ c/src/lib/libbsp/i386/virtPok/include/virtLayerCPU.h
@@ -0,0 +1,51 @@
+/* Date:      06/19/2013
+ * Author:    Philipp Eppelt
+ * Purpose:   CPU part of the virtualization layer.
+ * Licencse:  see RTEMS License.
+ */
+
+
+#ifndef RTEMS_VIRT_LAYER_CPU_H
+#define RTEMS_VIRT_LAYER_CPU_H
+
+/* Startup functions */
+
+int 
+virt_getWorkSpaceArea();
+
+
+
+/* Interrupts */
+int
+virt_requestIrq( int vector );
+
+void
+virt_detachIrq( int vector );
+
+void
+virt_enableInterrupts( int _level );
+
+void
+virt_disableInterrupts( int _level );
+
+void 
+virt_flashInterrupts( int _level );
+
+int
+virt_getInterruptLevel( void );
+
+
+
+/* Idle Thread */
+
+/**
+ * @brief Lowest priority thread, doing nothing, never returns;
+ */
+
+void
+virt_idleThread( void );
+
+
+
+
+#endif /* RTEMS_VIRT_LAYER_CPU_H */
